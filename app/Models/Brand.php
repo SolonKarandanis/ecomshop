71624b8fa5ee6c7c637ebbeeebbdee5f6304b9bb
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-12-24 09:39:14 +0200

    Crated Brand filament resources

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * @property int $id
@@ -34,5 +35,11 @@ class Brand extends Model
 
     public function products():HasMany{
         return $this->hasMany(Product::class);
+    }
+
+    public function getThumbnailImage():string
+    {
+        $isUrl=str_contains($this->image,'http');
+        return ($isUrl) ? $this->image : Storage::disk('public')->url($this->image);
     }
 }
