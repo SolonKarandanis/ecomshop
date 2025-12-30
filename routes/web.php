54288480dd1b014be812c104d452c025677cbe94
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-12-30 09:49:18 +0200

    Implementing page design

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,20 @@
 <?php
 
+use App\Livewire\CartPage;
 use App\Livewire\CategoriesPage;
+use App\Livewire\CheckoutPage;
 use App\Livewire\HomePage;
+use App\Livewire\MyOrdersPage;
+use App\Livewire\OrderDetailsPage;
+use App\Livewire\ProductDetailPage;
 use App\Livewire\ProductsPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomePage::class);
 Route::get('/categories', CategoriesPage::class);
 Route::get('/products', ProductsPage::class);
+Route::get('/products/{id}', ProductDetailPage::class);
+Route::get('/cart', CartPage::class);
+Route::get('/checkout', CheckoutPage::class);
+Route::get('/my-orders', MyOrdersPage::class);
+Route::get('/my-orders/{id}', OrderDetailsPage::class);
