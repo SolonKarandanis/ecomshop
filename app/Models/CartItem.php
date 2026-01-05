40bbb23db2f4344ffd049884fad722934acda0f0
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2026-01-05 11:35:52 +0200

    Created Cart and CartItems models and migrations

---
+++ app/Models/CartItem.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class CartItem extends Model
+{
+    use HasFactory;
+
+    protected $table = 'cart_items';
+
+    protected $fillable = [
+        'cart_id',
+        'product_id',
+        'quantity',
+        'unit_price',
+        'total_price',
+        'attributes',
+    ];
+
+    public function cart():BelongsTo{
+        return $this->belongsTo(Cart::class);
+    }
+
+    public function product():BelongsTo{
+        return $this->belongsTo(Product::class);
+    }
+}
