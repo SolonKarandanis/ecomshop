b0164d982c58223ca0edc0ba6ab7eff0a46b8bd8
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2026-03-11 06:25:43 +0200

    Added sweet alert

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Filament\Support\Assets\Js;
+use Filament\Support\Facades\FilamentAsset;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +24,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useTailwind();
+        FilamentAsset::register([
+            // Local asset build using Vite
+            Js::make('sweetalert2', Vite::asset('resources/js/sweetalert2.js')),
+        ]);
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,10 @@ import './product-detail-page';
 
 import 'preline'
 
+import Swal from 'sweetalert2'
+
+window.Swal = Swal
+
 document.addEventListener('livewire:navigated', () => {
     window.HSStaticMethods.autoInit();
 })
