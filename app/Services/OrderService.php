ace30cc716c43b017b48e5cd6b961ff034d12d0f
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2026-04-01 07:18:18 +0300

    Implementing Order functionality
    Refactoring code

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -8,7 +8,8 @@ use App\Models\Order;
 use App\Models\OrderItem;
 use App\Repositories\AddressRepository;
 use App\Repositories\OrderRepository;
-use Stripe\Checkout\Session;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Stripe\Stripe;
 
 class OrderService
@@ -20,59 +21,65 @@ class OrderService
         private readonly StripeService $stripeService
     ){}
 
+    /**
+     * @throws \Throwable
+     */
     public function checkout(CheckoutDTO $dto):string{
-        $cart = $this->cartService->getCart();
         $line_items=[];
         $order_items=[];
-        foreach ($cart->cartItems as $cartItem){
-            $line_items[]=[
-                'price_data'=>[
-                    'currency'=>config('app.currency'),
-                    'unit_amount'=>$cartItem->total_price * 100, //stripe wants unit amount in cents
-                    'product_data'=>[
-                        'name'=>$cartItem->product->name,
-                    ]
-                ],
-                'quantity'=>$cartItem->quantity,
-            ];
-            $order_items[]=[new OrderItem($cartItem)];
+        $paymentMethod=$dto->getPaymentMethod();
+        DB::beginTransaction();
+        try{
+            $cart = $this->cartService->getCart();
+            foreach ($cart->cartItems as $cartItem){
+                $line_item=[
+                    'price_data'=>[
+                        'currency'=>config('app.currency'),
+                        'unit_amount'=>$cartItem->total_price * 100, //stripe wants unit amount in cents
+                        'product_data'=>[
+                            'name'=>$cartItem->product->name,
+                        ]
+                    ],
+                    'quantity'=>$cartItem->quantity,
+                ];
+                $line_items[] = $line_item;
+                $order_items[]=[new OrderItem($cartItem)];
+            }
+            $order = new Order();
+            $order->user_id = auth()->user()->id;
+            $order->grand_total= $cart->total_price;
+            $order->payment_method = $paymentMethod;
+            $order->payment_status = 'pending';
+            $order->order_status=OrderStatusEnum::Draft->value;
+            $order->currency = 'eur';
+            $order->shipping_amount=0;
+            $order->shipping_method='none';
+            $order->notes='Order placed'.auth()->user()->name;
+            $order->setRelation('orderItems',$order_items);
+
+            $redirect_url = '';
+            if($paymentMethod=='stripe'){
+                Stripe::setApiKey(config('app.stripe_secret_key'));
+                $sessionCheckout = $this->stripeService->createSession($line_items);
+                $redirect_url=$sessionCheckout->url;
+            }
+
+            if($paymentMethod=='cod'){
+                $redirect_url=route('success');
+            }
+
+            $order->save();
+
+            $this->addressRepository->create($order->id,$dto);
+
+            $this->cartService->clearCart();
+            DB::commit();
+            return $redirect_url;
         }
-        $order = new Order();
-        $order->user_id = auth()->user()->id;
-        $order->grand_total= $cart->total_price;
-        $order->payment_method = $dto->getPaymentMethod();
-        $order->payment_status = 'pending';
-        $order->order_status=OrderStatusEnum::Draft->value;
-        $order->currency = 'eur';
-        $order->shipping_amount=0;
-        $order->shipping_method='none';
-        $order->notes='Order placed'.auth()->user()->name;
-        $order->setRelation('orderItems',$order_items);
-
-        $redirect_url = '';
-        if($validated['paymentMethod']=='stripe'){
-            Stripe::setApiKey(config('app.stripe_secret_key'));
-            $sessionCheckout = Session::create([
-                'payment_method_types' => ['card'],
-                'customer_email' => auth()->user()->email,
-                'line_items'=>$line_items,
-                'mode'=>'payment',
-                'success_url'=>route('success').'?session_id={CHECKOUT_SESSION_ID}',
-                'cancel_url'=>route('cancel'),
-            ]);
-            $redirect_url=$sessionCheckout->url;
+        catch (\Exception $exception){
+            Log::error($exception);
+            DB::rollBack();
+            return back()->with('error',$exception->getMessage()? :'Something went wrong!');
         }
-
-        if($validated['paymentMethod']=='cod'){
-            $redirect_url=route('success');
-        }
-
-        $order->save();
-
-        $this->addressRepository->create($order->id,$dto);
-
-        $this->cartService->clearCart();
-
-        return $redirect_url;
     }
 }
--- app/Services/StripeService.php
+++ app/Services/StripeService.php
@@ -2,7 +2,26 @@
 
 namespace App\Services;
 
+use Stripe\Checkout\Session;
+use Stripe\Exception\ApiErrorException;
+use Stripe\Stripe;
+
 class StripeService
 {
 
+    /**
+     * @throws ApiErrorException
+     */
+    public function createSession(array $line_items):Session{
+        /** @var array[] $line_items */
+        Stripe::setApiKey(config('app.stripe_secret_key'));
+        return Session::create([
+            'payment_method_types' => ['card'],
+            'customer_email' => (string) auth()->user()->email,
+            'line_items'=>$line_items,
+            'mode'=>'payment',
+            'success_url'=>route('success').'?session_id={CHECKOUT_SESSION_ID}',
+            'cancel_url'=>route('cancel'),
+        ]);
+    }
 }
