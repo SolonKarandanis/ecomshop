ff3fc0c93f72affb461e63e222af79318e1f7f50
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2026-04-07 07:45:00 +0300

    Implementing success page

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -81,6 +82,13 @@ class Order extends Model
     public function stripeOrderDetail(): HasOne
     {
         return $this->hasOne(StripeOrderDetail::class);
+    }
+
+    public function createdAtDiff():Attribute
+    {
+        return Attribute::make(
+            get:fn()=> $this->created_at->diffForHumans()
+        );
     }
 
 }
