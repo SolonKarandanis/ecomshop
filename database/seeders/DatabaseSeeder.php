444b81f88687b730f087d04542946120e6665ca2
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2026-04-10 07:06:28 +0300

    Adding an admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $adminUser = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'skarandanis@gmail.com',
+            'password' => bcrypt('7ujm&UJM'),
         ]);
 
         $this->call(PaymentMethodSeeder::class);
