f5cd25527a383b5f9cc1835a15fceeb466774ecc
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2026-04-22 18:02:01 +0300

    I18n functionality
    Added translations in navbar

---
+++ lang/en/navbar.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'home'=>'Home',
+    'categories'=>'Categories',
+    'products'=>'Products',
+    'cart'=>'Cart',
+    'my_orders'=>'My Orders',
+    'my_account'=>'My Account',
+    'logout'=>'Logout',
+    'buttons'=>[
+        'log_in'=>'Log In',
+    ]
+];
--- resources/views/livewire/partials/navbar.blade.php
+++ resources/views/livewire/partials/navbar.blade.php
@@ -22,14 +22,14 @@
                 <div class="overflow-y-auto max-h-[75vh] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-slate-700 dark:[&::-webkit-scrollbar-thumb]:bg-slate-500">
                     <div class="flex flex-col gap-x-0 mt-5 divide-y divide-dashed divide-gray-200 md:flex-row md:items-center md:justify-end md:gap-x-7 md:mt-0 md:ps-7 md:divide-y-0 md:divide-solid dark:divide-gray-700">
 
-                        <x-nav-link href="{{route('home')}}" :active="request()->routeIs('home')">Home</x-nav-link>
-                        <x-nav-link href="{{route('categories')}}" :active="request()->routeIs('categories')">Categories</x-nav-link>
-                        <x-nav-link href="{{route('products')}}" :active="request()->routeIs('products')">Products</x-nav-link>
+                        <x-nav-link href="{{route('home')}}" :active="request()->routeIs('home')">{{__('navbar.home')}}</x-nav-link>
+                        <x-nav-link href="{{route('categories')}}" :active="request()->routeIs('categories')">{{__('navbar.categories')}}</x-nav-link>
+                        <x-nav-link href="{{route('products')}}" :active="request()->routeIs('products')">{{__('navbar.products')}}</x-nav-link>
                         <x-nav-link href="{{route('cart')}}" :active="request()->routeIs('cart')" class="flex items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="shrink-0 w-5 h-5 mr-1">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                             </svg>
-                            <span class="mr-1">Cart</span>
+                            <span class="mr-1">{{__('navbar.cart')}}</span>
                             <span class="py-0.5 px-1.5 rounded-full text-xs font-medium bg-blue-50 border border-blue-200 text-blue-600">{{$this->total_cart_items}}</span>
                         </x-nav-link>
 
@@ -40,7 +40,7 @@
                                     <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
                                     <circle cx="12" cy="7" r="4" />
                                 </svg>
-                                Log in
+                                {{__('navbar.buttons.log_in')}}
                             </a>
                         </div>
                         @endguest
@@ -59,18 +59,18 @@
                                    class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2
                                         focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:focus:outline-none
                                         dark:focus:ring-1 dark:focus:ring-gray-600" >
-                                    My Orders
+                                    {{__('navbar.my_orders')}}
                                 </a>
 
                                 <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2
                                         focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:focus:outline-none dark:focus:ring-1
                                         dark:focus:ring-gray-600" >
-                                    My Account
+                                    {{__('navbar.my_account')}}
                                 </a>
                                 <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2
                                 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:focus:outline-none
                                 dark:focus:ring-1 dark:focus:ring-gray-600" href="/logout">
-                                    Logout
+                                    {{__('navbar.logout')}}
                                 </a>
                             </div>
                         </div>
