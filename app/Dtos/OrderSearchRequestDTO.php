ffc16c50df25af2b483ff19fa8f8ac0e32bf5792
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2026-05-12 07:29:24 +0300

    Implementing export orders functionality

---
+++ app/Dtos/OrderSearchRequestDTO.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Dtos;
+
+class OrderSearchRequestDTO
+{
+    private int $userId;
+    private ?string $orderStatus = null;
+    private ?string $paymentStatus = null;
+    private ?string $fromDate = null;
+    private ?string $toDate = null;
+    private int $perPage = 5;
+
+    public function __construct()
+    {
+        $this->userId = auth()->user()->id;
+    }
+
+    public function withOrderStatus(string $orderStatus): self
+    {
+        $this->orderStatus = $orderStatus;
+        return $this;
+    }
+
+    public function withPaymentStatus(string $paymentStatus): self
+    {
+        $this->paymentStatus = $paymentStatus;
+        return $this;
+    }
+
+    public function withFromDate(string $fromDate): self
+    {
+        $this->fromDate = $fromDate;
+        return $this;
+    }
+
+    public function withToDate(string $toDate): self
+    {
+        $this->toDate = $toDate;
+        return $this;
+    }
+
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+
+    public function getOrderStatus(): ?string
+    {
+        return $this->orderStatus;
+    }
+
+    public function getPaymentStatus(): ?string
+    {
+        return $this->paymentStatus;
+    }
+
+    public function getFromDate(): ?string
+    {
+        return $this->fromDate;
+    }
+
+    public function getToDate(): ?string
+    {
+        return $this->toDate;
+    }
+
+    public function getPerPage(): int
+    {
+        return $this->perPage;
+    }
+}
